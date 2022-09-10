c3e81ac8660cfdc3fbf9baff08dd9ea5145ee013
Yrimar Cabaniel <yrimarcg@gmail.com>
2022-09-10 10:52:41 -0400

    agrege el modelo de resrva

--- apps/reserva/models.py
+++ apps/reserva/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Reserva(models.Model):
+    fecha_ingreso = models.CharField(max_length=20)
+    fecha_salida = models.CharField(max_length=20)
+    cantidad_personas = models.IntegerField()
